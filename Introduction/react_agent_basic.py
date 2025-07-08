3dc3054545f1ae4a67d9accf62a9b67c413b5fe0
samsul jahith s <115388971+samsuljahith@users.noreply.github.com>
2025-07-08 12:52:25 +0530

    im created agent with tools

--- Introduction/react_agent_basic.py
+++ Introduction/react_agent_basic.py
@@ -1,8 +1,18 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
+from langchain.agents import initialize_agent,tool,AgentType
+
+
 
 load_dotenv()
-
+@tool
+def web_search_tool(query: str) -> str:
+    """Search the web for the given query and return results."""
+    # Placeholder for web search functionality
+    return f"Results for '{query}' from web search."
+tools=[web_search_tool]
 llm = ChatGroq(model="compound-beta", temperature=0)
+agent= initialize_agent(llm=llm, tools=tools, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
 
-print(llm.invoke("What is the capital of France?"))
+result=agent.invoke({"input":"Give me a tweet about today wheather in chennai"})
+print(result)
